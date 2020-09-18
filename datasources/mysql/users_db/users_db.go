2ff52a6404c8a9b637adb07dba7f9a0b46d3177f
yossefazoulay <azoulayos@protonmail.com>
2020-09-18 12:54:07 +0200

    Mysql Clinet Init

---
+++ datasources/mysql/users_db/users_db.go
@@ -0,0 +1,43 @@
+package users_db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	_ "github.com/go-sql-driver/mysql"
+	"os"
+)
+
+const (
+	mysql_users_username = "mysql_users_username"
+	mysql_users_pw       = "mysql_users_pw"
+	mysql_users_host     = "mysql_users_host"
+	mysql_users_schema   = "mysql_users_schema"
+)
+
+var (
+	Client   *sql.DB
+	username = os.Getenv(mysql_users_username)
+	password = os.Getenv(mysql_users_pw)
+	host     = os.Getenv(mysql_users_host)
+	schema   = os.Getenv(mysql_users_schema)
+)
+
+func init() {
+	datasourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
+		username,
+		password,
+		host,
+		schema,
+	)
+	var connErr error
+	Client, connErr = sql.Open("mysql", datasourceName)
+	if connErr != nil {
+		panic(connErr)
+	}
+	if connErr = Client.Ping(); connErr != nil {
+		panic(connErr)
+	}
+	log.Println("database successfully configured")
+
+}
