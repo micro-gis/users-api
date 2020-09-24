5e04246ffe54b155a20026c27d8e79b9789f2293
yossefazoulay <azoulayos@protonmail.com>
2020-09-24 17:07:27 +0200

    Handle mysql errore

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,21 +1,21 @@
 package users
 
 import (
-	"fmt"
 	"github.com/micro-gis/users-api/datasources/mysql/users_db"
 	"github.com/micro-gis/users-api/utils/date"
 	"github.com/micro-gis/users-api/utils/errors"
-	"strings"
+	"github.com/micro-gis/users-api/utils/mysql_utils"
 )
 
 const (
-	indexUniqueEmail = "users_email_uindex"
-	errNoRows        = "no rows in result set"
-	queryInsertUser  = "INSERT INTO users (first_name, last_name, email, date_created) VALUES (?,?,?,?);"
-	queryGetUser     = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id = ?;"
+	queryInsertUser = "INSERT INTO users (first_name, last_name, email, date_created) VALUES (?,?,?,?);"
+	queryGetUser    = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id = ?;"
 )
 
 func (user *User) Get() *errors.RestErr {
+	if connErr := users_db.Client.Ping(); connErr != nil {
+		panic(connErr)
+	}
 	stmt, err := users_db.Client.Prepare(queryGetUser)
 	if err != nil {
 		return errors.NewInternalServerError(err.Error())
@@ -24,14 +24,7 @@ func (user *User) Get() *errors.RestErr {
 
 	result := stmt.QueryRow(user.Id)
 	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
-		if strings.Contains(err.Error(), errNoRows) {
-			return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
-		}
-		return errors.NewInternalServerError(fmt.Sprintf("error when trying to get user : %d : %s", user.Id, err.Error()))
-	}
-
-	if connErr := users_db.Client.Ping(); connErr != nil {
-		panic(connErr)
+		return mysql_utils.ParseError(err)
 	}
 
 	return nil
@@ -47,14 +40,11 @@ func (user *User) Save() *errors.RestErr {
 	user.DateCreated = date.GetNowString()
 	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
 	if err != nil {
-		if strings.Contains(err.Error(), indexUniqueEmail) {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", user.Email))
-		}
-		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to save user : %s\n ", err.Error()))
+		return mysql_utils.ParseError(err)
 	}
 	userId, err := insertResult.LastInsertId()
 	if err != nil {
-		return errors.NewInternalServerError(fmt.Sprintf("error trying to retrieve last user id : %s\n", err.Error()))
+		return mysql_utils.ParseError(err)
 	}
 	user.Id = userId
 	return nil
---
+++ utils/mysql_utils/mysql_utils.go
@@ -0,0 +1,27 @@
+package mysql_utils
+
+import (
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"github.com/micro-gis/users-api/utils/errors"
+	"strings"
+)
+
+const (
+	errNoRows = "no rows in result set"
+)
+
+func ParseError(err error) *errors.RestErr {
+	sqlErr, ok := err.(*mysql.MySQLError)
+	if !ok {
+		if strings.Contains(err.Error(), errNoRows) {
+			return errors.NewNotFoundError(fmt.Sprintf("no record matching giver id"))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to save user : %s\n ", err.Error()))
+	}
+	switch sqlErr.Number {
+	case 1062:
+		return errors.NewBadRequestError(fmt.Sprintf("email already exists"))
+	}
+	return errors.NewInternalServerError("error processing request")
+}
